d142231d5f492916e3d7dec185049d5a23bc591f
Viraj Bhambure <virajbhambure123@gmail.com>
2025-01-20 19:10:15 +0530

    some minor changes

--- src/utils/apiResponce.js
+++ src/utils/apiResponce.js
@@ -1,8 +1,10 @@
 class apiResponce{
     constructor(statusCode,data,massage="Success")
     {
+        
         this.statusCode=statusCode;
         this.data=data;
+        // console.log(this.data)
         this.massage=massage;
         this.success=statusCode <400;
     }
