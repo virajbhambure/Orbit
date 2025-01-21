c0d65aa4d2a56164a7571e29c9c4857eaea59e30
Viraj Bhambure <virajbhambure123@gmail.com>
2025-01-21 21:32:18 +0530

    bug solving in video routs

--- src/app.js
+++ src/app.js
@@ -1,26 +1,30 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+const app = express();
 
-const app= express();
-
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
-//when data will come from differnt sources such as in form of json,from urls, in form of pictures etc for that 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+//when data will come from differnt sources such as in form of json,from urls, in form of pictures etc for that
 //we use middlewares and middlewares are use as "app.use()"  like this
-app.use(express.json())
-app.use(express.urlencoded({extended:true }))
-app.use(express.static("public"))
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //Routes importing
-import userRouter from "./routes/user.routes.js"
-
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js"
 //routes decleration
-app.use("/api/v1/users",userRouter);    //we have saperated routes so for using it we neet to use middlewares so we used app.use()
+
+app.use("/api/v1/users", userRouter);
+//we have saperated routes so for using it we neet to use middlewares so we used app.use()
+
+app.use("/api/v1/videos", videoRouter)
 // http://localhost:8000/api/v1/users/register
-export { app }
+export { app };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,43 +1,40 @@
 import { v2 as cloudinary } from "cloudinary";
-import fs from 'fs'
-
+import fs from "fs";
 
 // Configuration
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:  process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary= async(localFilePath)=>{
-    try {
-        if(!localFilePath)return null
-        //upload the file on cloudinary
-       const responce=await cloudinary.uploader.upload(localFilePath,{resource_type:'auto'})
-        //file uploaded succesfully 
-        // console.log("File uploaded succesfully",responce.url);
-        fs.unlinkSync(localFilePath);   //files will be removed if uploaded or also when not uploaded properly
-        return responce;
-        
-    } catch (error) {
-        fs.unlinkSync(localFilePath) //remove the locally saved file as the upload operation got failed
-        return null;
-        
-    }
-
-
-}
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const responce = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file uploaded succesfully
+    // console.log("File uploaded succesfully",responce.url);
+    fs.unlinkSync(localFilePath); //files will be removed if uploaded or also when not uploaded properly
+    return responce;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saved file as the upload operation got failed
+    return null;
+  }
+};
 
 const deleteFromCloudinary = async (publicId) => {
-    try {
-        if (!publicId) throw new Error('No public ID provided for deletion');
-        // Perform delete operation on Cloudinary
-        const result = await cloudinary.uploader.destroy(publicId);
-        return result;
-    } catch (error) {
-        console.error("Error deleting from Cloudinary:", error);
-        return null;
-    }
-}
+  try {
+    if (!publicId) throw new Error("No public ID provided for deletion");
+    // Perform delete operation on Cloudinary
+    const result = await cloudinary.uploader.destroy(publicId);
+    return result;
+  } catch (error) {
+    console.error("Error deleting from Cloudinary:", error);
+    return null;
+  }
+};
 
-export{uploadOnCloudinary,deleteFromCloudinary}
+export { uploadOnCloudinary, deleteFromCloudinary };
