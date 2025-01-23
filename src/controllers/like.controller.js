201f6a3eb7246c6cf8c9035fc957c33ea2bebc6a
Viraj Bhambure <virajbhambure123@gmail.com>
2025-01-23 08:59:10 +0530

    implement like functionality for videos, comments, and tweets

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,151 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { user } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { apiResponce } from "../utils/apiResponce.js";
+import jwt from "jsonwebtoken";
+import mongoose ,{isValidObjectId}from "mongoose";
+
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+    const {videoId} = req.params
+    //TODO: toggle like on video
+    if(!isValidObjectId(videoId))
+    {
+        throw new apiError(404,"Invalid videoId");
+    }
+
+    //check if like exist
+    const isExist=await like.findOne(
+        {
+            video:videoId,
+            likedBy:req.user._id;
+        }
+    )
+
+    if(isExist)
+    {
+        await isExist.remove();
+        return res
+        .status(200)
+        .json(new apiResponce(200,"unliked sucessfully"))
+
+    }
+    else
+    {   
+        await like.create(
+            {
+                video:videoId,
+                likedBy:req.user._id;
+            }
+        )
+        return res
+        .status(200)
+        .json(
+            {
+                200,"Liked successfully"
+            }
+        )
+    }
+
+})
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    //TODO: toggle like on comment
+    if(!isValidObjectId(commentId))
+    {
+        
+    {
+        throw new apiError(404, "Invalid comment Id");
+    }}
+    //check if exist 
+    const existingLike= await like.findOne(
+        {
+            comment:commentId,
+            likedBy:req.user._id;
+        }
+    );
+    if (existingLike) {
+        await existingLike.remove();
+        res.status(200).json(apiResponce(200,"Comment unliked successfully"));
+    } else {
+        await like.create({
+            comment: commentId,
+            likedBy: req.user._id,
+        });
+        res.status(200).json(apiResponce(200,"Comment liked successfully"));
+    }
+
+})
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params
+    //TODO: toggle like on tweet
+    if(!isValidObjectId(tweetId))
+    {
+        throw new apiError(404,"invalid tweetId");
+    }
+    const likeExist= await like.findOne(
+        {
+            tweet:tweetId,
+            likedBy:req.user._id
+        }
+    )
+
+    if(likeExist)
+    {
+        const status=await likeExist.remove();
+        if(!status)
+        {
+            throw new apiError(500,"Error in removing like from tweet");
+        }
+        return res
+        .status(200)
+        .json(new apiResponce(200,"Tweet like removed successfully"));
+    }
+    const status = await like.create(
+        {
+            tweet:tweetId,
+            likedBy:req.user._id
+        }
+    )
+
+    // if(!status)
+    // {
+    //     throw new apiError(500,"Error in creating like to the tweet")
+    // }
+    return res
+    .status(200)
+    .json(new apiResponce(200,"Like added succesfully"));
+}
+)
+
+const getlikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+
+    const userId= req.user._id;
+    const likedVideos=await like.find(
+        {
+            likedBy:userId,
+            video:{$exist:true}
+        }
+    ).populate('video');  //it will show only videos liked by user
+    if(!likedVideos || likedVideos.length===0)
+    {
+        return res
+        status(404)
+        .json(new apiResponce(404,"No liked videos found"));
+    }
+
+    return res
+        .status(200)
+        .json(new apiResponce(200,"Liked videos fetched successfully", likedVideos));
+})
+
+export {
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+    getLikedVideos
+}
